4f8455e7adb2167547092708ff54b1b7729e4643
Hamid Moghaddam <hamid.moghaddam@R5004160L.local>
2017-06-02 14:00:43 +1200

    webhook.py v1.09

--- webhook.py
+++ webhook.py
@@ -32,18 +32,35 @@ def webhook():
     return r
 def processRequest(req):
     if req.get("result").get("action") == "news.search":
+        titles[:]=[]
+        contents[:]=[]
         return read_news_title()
     elif req.get("result").get("action") == "article.open":
-        return {}
+        return get_text(req)
     else:
         return {}
 def read_news_title():
     feed = feedparser.parse("http://www.stuff.co.nz/rss/")
-    titles=[]
+    
     for post in feed.entries:
         titles.append(post.title)
+        contents.append(post.content)
     res= makeWebhookResult(titles[0:3])
     return res
+def get_text(req):
+    result = req.get("result")
+    parameters = result.get("parameters")
+    number = parameters.get("number")
+    if number is None:
+        return None    
+    speech= contents[number]
+    return {
+        "speech": speech,
+        "displayText": speech,
+        "data": {},
+        "contextOut": [],
+        "source": "Fairfax Chatbot"
+    }    
 def makeWebhookResult(data):
     speech=""
     for i in range(0,len(data)):    
@@ -59,7 +76,8 @@ def makeWebhookResult(data):
         "source": "Fairfax Chatbot"
     }    
 if __name__ == '__main__':
-   
+    titles=[]
+    contents=[]
     port = int(os.getenv('PORT', 5000))
 
     print("Starting app on port %d" % port)
